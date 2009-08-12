f7d47b770f01dc5e4a1f4e69ab0c4dfc68803e15
klondike <kprojection@gmail.com>
2009-08-12 17:10:34 -0400

    Proved that the Sunlight API client works and can be integrated into the Android app

--- src/com/sunlightlabs/android/congress/LegislatorList.java
+++ src/com/sunlightlabs/android/congress/LegislatorList.java
@@ -1,13 +1,34 @@
 package com.sunlightlabs.android.congress;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sunlightlabs.api.ApiCall;
+import com.sunlightlabs.entities.Legislator;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LegislatorList extends Activity {
+	private ApiCall api;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Put API Key here
+        api = new ApiCall("");
+        
+        // Put zip code here
+        Legislator[] legislators = Legislator.getLegislatorsForZipCode(api, "");
+        
+        TextView debug = (TextView) this.findViewById(R.id.debug);
+        if (legislators.length > 0)
+        	debug.setText(legislators[0].getName());
+        else
+        	debug.setText("No legislators found");
     }
 }
