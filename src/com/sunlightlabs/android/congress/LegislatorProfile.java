80c011cd49b13e58e8150026ea83bb7ed9ec1517
klondike <kprojection@gmail.com>
2009-09-14 18:02:02 -0400

    Legislator profile pulls from GovPix for images

--- src/com/sunlightlabs/android/congress/LegislatorProfile.java
+++ src/com/sunlightlabs/android/congress/LegislatorProfile.java
@@ -1,6 +1,13 @@
 package com.sunlightlabs.android.congress;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -33,10 +40,33 @@ public class LegislatorProfile extends Activity {
 	
 	public void loadInformation() {
 		ImageView picture = (ImageView) this.findViewById(R.id.picture);
-		picture.setImageResource(R.drawable.no_photo);
+		
+		String url = "http://govpix.appspot.com/" + Uri.encode(legislator.picName());
+		InputStream stream;
+		Drawable drawable;
+		try {
+			stream = (InputStream) fetch(url);
+			drawable = Drawable.createFromStream(stream, "src");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+				
+		picture.setImageDrawable(drawable);
+		
 		
 		TextView name = (TextView) this.findViewById(R.id.profile_name);
 		name.setText(legislator.getName());
 	}
+	
+	public Object fetch(String address) throws MalformedURLException, IOException {
+		URL url = new URL(address);
+		Object content = url.getContent();
+		return content;
+	}
+
 	
 }
