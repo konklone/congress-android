c97afd2b018ade2c433296b6901e4cfe5d51a99e
klondike <kprojection@gmail.com>
2009-09-23 16:14:49 -0400

    Moved legislator loading out of the UI thread

--- src/com/sunlightlabs/android/congress/LegislatorList.java
+++ src/com/sunlightlabs/android/congress/LegislatorList.java
@@ -1,17 +1,22 @@
 package com.sunlightlabs.android.congress;
 
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView.OnItemClickListener;
 
+import com.sunlightlabs.android.congress.LegislatorNews.NewsAdapter;
 import com.sunlightlabs.api.ApiCall;
 import com.sunlightlabs.entities.Legislator;
 
 public class LegislatorList extends ListActivity {
+	private final static int LOADING = 0;
 	private Legislator[] legislators;
     
     @Override
@@ -19,22 +24,37 @@ public class LegislatorList extends ListActivity {
     	super.onCreate(savedInstanceState);
     	
     	loadLegislators();
-    	
-    	setListAdapter(new ArrayAdapter<Legislator>(this, android.R.layout.simple_list_item_1, legislators));
-    	getListView().setOnItemClickListener(new OnItemClickListener() { 
-    		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    			launchLegislator(((Legislator) parent.getItemAtPosition(position)).getId());
-    		}
-    	});
     }
     
+    final Handler handler = new Handler();
+    final Runnable updateThread = new Runnable() {
+        public void run() {
+        	setListAdapter(new ArrayAdapter<Legislator>(LegislatorList.this, android.R.layout.simple_list_item_1, legislators));
+        	getListView().setOnItemClickListener(new OnItemClickListener() { 
+        		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        			launchLegislator(((Legislator) parent.getItemAtPosition(position)).getId());
+        		}
+        	});
+        	dismissDialog(LOADING);
+        }
+    };
+	
     public void loadLegislators() {
-    	String api_key = getResources().getString(R.string.sunlight_api_key);
-		ApiCall api = new ApiCall(api_key);
-		
-		// expand here to handle other types of legislator searches for this list
-    	String zipCode = getIntent().getStringExtra("zip_code");
-    	legislators = Legislator.getLegislatorsForZipCode(api, zipCode);
+    	Thread loadingThread = new Thread() {
+	        public void run() {
+		    	String api_key = getResources().getString(R.string.sunlight_api_key);
+				ApiCall api = new ApiCall(api_key);
+				
+				// expand here to handle other types of legislator searches for this list
+		    	String zipCode = getIntent().getStringExtra("zip_code");
+		    	legislators = Legislator.getLegislatorsForZipCode(api, zipCode);
+		    	
+		    	handler.post(updateThread);
+	        }
+    	};
+    	loadingThread.start();
+	    
+		showDialog(LOADING);
     }
     
     public void launchLegislator(String id) {
@@ -46,5 +66,17 @@ public class LegislatorList extends ListActivity {
 		i.putExtras(extras);
 		
 		startActivity(i);
+    }
+    
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+        case LOADING:
+            ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setMessage("Finding legislators...");
+            return dialog;
+        default:
+            return null;
+        }
     }
 }
