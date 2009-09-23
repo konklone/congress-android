b005d224ff2f87385fa55969b435f63b86821fb8
klondike <kprojection@gmail.com>
2009-09-23 16:07:48 -0400

    Moved news gathering out of the UI thread and added a progress dialog

--- src/com/sunlightlabs/android/congress/LegislatorNews.java
+++ src/com/sunlightlabs/android/congress/LegislatorNews.java
@@ -1,9 +1,12 @@
 package com.sunlightlabs.android.congress;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +21,8 @@ import com.sunlightlabs.android.yahoo.news.NewsItem;
 import com.sunlightlabs.android.yahoo.news.NewsService;
 
 public class LegislatorNews extends ListActivity {
+	private static final int LOADING = 0;
+	
 	private String searchName;
 	private NewsItem[] items;
     	
@@ -30,17 +35,43 @@ public class LegislatorNews extends ListActivity {
     	loadNews();
 	}
 	
-	public void loadNews() {
-		String apiKey = getResources().getString(R.string.yahoo_news_key);
-		NewsService service = new NewsService(apiKey);
-		
-		try {
-			items = service.fetchNewsResults(searchName);
-		} catch (NewsException e) {
-			Toast.makeText(this, "Couldn't load news, try again later.", Toast.LENGTH_SHORT);
-		}
-		setListAdapter(new NewsAdapter(LegislatorNews.this, items));
+	final Handler handler = new Handler();
+    final Runnable updateNews = new Runnable() {
+        public void run() {
+        	setListAdapter(new NewsAdapter(LegislatorNews.this, items));
+        	dismissDialog(LOADING);
+        }
+    };
+	
+	protected void loadNews() {
+		Thread newsThread = new Thread() {
+	        public void run() { 
+	        	try {
+	    			String apiKey = getResources().getString(R.string.yahoo_news_key);
+	    			NewsService service = new NewsService(apiKey);
+	    			items = service.fetchNewsResults(searchName);
+	    		} catch (NewsException e) {
+	    			Toast.makeText(LegislatorNews.this, "Couldn't load news, try again later.", Toast.LENGTH_SHORT);
+	    		}
+	        	handler.post(updateNews);
+	        }
+	    };
+	    newsThread.start();
+	    
+		showDialog(LOADING);
 	}
+    
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+        case LOADING:
+            ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setMessage("Plucking news from the air...");
+            return dialog;
+        default:
+            return null;
+        }
+    }
 	
 	public NewsItem[] fakeNews() {
 		NewsItem[] items = new NewsItem[5];
