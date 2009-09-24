c022066020993655df630d7971c095ebfe3d1051
klondike <kprojection@gmail.com>
2009-09-24 18:57:14 -0400

    Grey out Location button if there is no last known location

--- src/com/sunlightlabs/android/congress/MainMenu.java
+++ src/com/sunlightlabs/android/congress/MainMenu.java
@@ -1,25 +1,37 @@
 package com.sunlightlabs.android.congress;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainMenu extends Activity {
 	public static final int RESULT_ZIP = 1;
+	private Location location;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        loadLocation();
         setupControls();
     }
+	
+	public void loadLocation() {
+		LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+		location = null;
+		List<String> enabled = lm.getProviders(true);
+		if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
+			location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		else if (lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+			location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+	}
 	
 	public void setupControls() {
         Button fetchZip = (Button) this.findViewById(R.id.fetch_zip);
@@ -33,21 +45,15 @@ public class MainMenu extends Activity {
     		}
     	});
     	
-    	fetchLocation.setOnClickListener(new View.OnClickListener() {
-    		public void onClick(View v) {
-    			LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-    			Location location = null;
-    			if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
-    				location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-    			else if (lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
-    				location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-    			else
-    				Toast.makeText(MainMenu.this, "Cannot detect your location. Visit your location settings to turn on WiFi or GPS location detection.", Toast.LENGTH_LONG).show();
-    			
-    			if (location != null)
-    				searchByLatLong(location.getLatitude(), location.getLongitude());
-    		}
-    	});
+    	if (location != null) {
+	    	fetchLocation.setOnClickListener(new View.OnClickListener() {
+	    		public void onClick(View v) {
+	    			if (location != null)
+	    				searchByLatLong(location.getLatitude(), location.getLongitude());
+	    		}
+	    	});
+    	} else
+    		fetchLocation.setEnabled(false);
     }
 	
 	public void searchByZip(String zipCode) {
