61ee26124e6a29baedf7f9b47e01d6295d7d88ca
klondike <kprojection@gmail.com>
2009-09-28 16:13:18 -0400

    An image caching solution that works for small and medium pictures but for large pictures

--- src/com/sunlightlabs/android/congress/LegislatorProfile.java
+++ src/com/sunlightlabs/android/congress/LegislatorProfile.java
@@ -1,6 +1,12 @@
 package com.sunlightlabs.android.congress;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
@@ -19,7 +25,7 @@ public class LegislatorProfile extends Activity {
 	private String id, titledName, party, state, domain, phone, website, office;
 	private Drawable avatar;
 	
-	private String avatarPath = "/sdcard/sunlight-android/avatars";
+	private static final String avatarPath = "/sdcard/sunlight-android/avatars/";
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,8 +42,6 @@ public class LegislatorProfile extends Activity {
         phone = extras.getString("phone");
         website = extras.getString("website");
         office = extras.getString("office");
-        
-        initializeDirectories(id);
         
         loadInformation();
         loadImage();
@@ -89,39 +93,101 @@ public class LegislatorProfile extends Activity {
 		loadingThread.start();
 	}
 	
-	private Object fetchObject(String address) {
+	/*
+	 * Various static methods that other classes can use to fetch legislator profile images,
+	 * and cause them to be cached to disk.
+	 */
+	
+	public static Drawable getImage(String size, String bioguideId) {
+		initializeDirectories(bioguideId);
+		
+		File imageFile = new File(picPath(size, bioguideId));
+		if (!imageFile.exists())
+			cacheImages(bioguideId);
+		
+		InputStream stream;
+		try {
+			stream = new FileInputStream(imageFile);
+		}
+		catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		
+		Drawable drawable = Drawable.createFromStream(stream, "src");
+		return drawable;
+	}
+	
+	private static void initializeDirectories(String bioguideId) {
+		File avatarDir = new File(avatarPath);
+		if (!avatarDir.exists())
+			avatarDir.mkdirs();
+		
+		File legislatorDir = new File(picDir(bioguideId));
+		if (!legislatorDir.exists())
+			legislatorDir.mkdir();
+	}
+	
+	private static String picUrl(String size, String bioguideId) {
+		return "http://assets.sunlightfoundation.com/moc/" + size + "/" + bioguideId + ".jpg";
+	}
+	
+	private static String picPath(String size, String bioguideId) {
+		return picDir(bioguideId) + size + ".jpg"; 
+	}
+	
+	private static String picDir(String bioguideId) {
+		return avatarPath + bioguideId + "/";
+	}
+	
+	private static void cacheImages(String bioguideId) {
+		cacheImage(PIC_SMALL, bioguideId);
+		cacheImage(PIC_MEDIUM, bioguideId);
+		cacheImage(PIC_LARGE, bioguideId);
+	}
+	
+	private static void cacheImage(String size, String bioguideId) {
+		File outFile = new File(picPath(size, bioguideId));
+		if (outFile.exists())
+			return;
+		
+		String url = picUrl(size, bioguideId);
+		InputStream stream = fetchStream(url);
+		if (stream != null)
+			writeFile(stream, outFile);
+	}
+	
+	private static InputStream fetchStream(String address) {
 		try {
 			URL url = new URL(address);
-			return url.getContent();
+			return ((InputStream) url.getContent());
 		} catch (Exception e) {
 			return null;
 		}
 	}
 	
-	private void initializeDirectories(String id) {
-		File avatarDir = new File(avatarPath + "/");
-		if (!avatarDir.exists())
-			avatarDir.mkdirs();
-		
-		File legislatorDir = new File(avatarPath + "/" + id + "/");
-		if (!legislatorDir.exists())
-			legislatorDir.mkdir();
+	private static void writeFile(InputStream stream, File outputFile) {
+		try {
+			BufferedOutputStream fos = new BufferedOutputStream(new FileOutputStream(outputFile));
+			
+	        int bytesAvailable = stream.available();
+	        int maxBufferSize = 1024 * 4;
+	        int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+	        byte[] buffer = new byte[bufferSize];
+	        int bytesRead = stream.read(buffer, 0, bufferSize);
+	        while (bytesRead > 0) {
+	            fos.write(buffer, 0, bufferSize);
+	            bytesAvailable = stream.available();
+	            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+	            bytesRead = stream.read(buffer, 0, bufferSize);
+	        }
+	        stream.close();
+	        fos.flush();
+	        fos.close();
+		} catch(FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
-	
-	private String picUrl(String size, String bioguideId) {
-		return "http://assets.sunlightfoundation.com/moc/" + size + "/" + bioguideId + ".jpg";
-	}
-	
-	/**
-	 * 
-	 */
-	public Drawable getImage(String size, String bioguideId) {
-		String url = picUrl(PIC_MEDIUM, id);
-		
-		InputStream stream = (InputStream) fetchObject(url);
-		return Drawable.createFromStream(stream, "src");
-	}
-	
-	
 	
 }
