7435f48d5b53e3bbed68d83c8aabe1e714f1293f
klondike <kprojection@gmail.com>
2009-09-29 12:43:40 -0400

    Rearranged info, displaying full party and state names

--- src/com/sunlightlabs/android/congress/LegislatorProfile.java
+++ src/com/sunlightlabs/android/congress/LegislatorProfile.java
@@ -57,28 +57,24 @@ public class LegislatorProfile extends Activity {
     };
 	
 	public void loadInformation() {		
-		// avatar (loading photo)
 		ImageView photo = (ImageView) this.findViewById(R.id.picture);
 		photo.setImageResource(R.drawable.loading_photo);
 		
-		// name
 		TextView name = (TextView) this.findViewById(R.id.profile_name);
 		name.setText(titledName);
 		
-		// party and state
-		TextView party_state = (TextView) this.findViewById(R.id.profile_party_state);
-		String party_line = "(" + party + "-" + state + ") " + domain; 
-		party_state.setText(party_line);
+		TextView partyView = (TextView) this.findViewById(R.id.profile_party); 
+		partyView.setText(partyName(party));
+		
+		TextView stateView = (TextView) this.findViewById(R.id.profile_state); 
+		stateView.setText(stateName(state));
+		
+		TextView domainView = (TextView) this.findViewById(R.id.profile_domain); 
+		domainView.setText(domain);
 
-		// phone
 		TextView phoneView = (TextView) this.findViewById(R.id.profile_phone);
 		phoneView.setText(phone);
 		
-		// office address
-		TextView officeView = (TextView) this.findViewById(R.id.profile_office);
-		officeView.setText(office);
-		
-		// website
 		TextView websiteView = (TextView) this.findViewById(R.id.profile_website);
 		websiteView.setText(website);
 	}
@@ -180,6 +176,126 @@ public class LegislatorProfile extends Activity {
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	private String partyName(String code) {
+		if (code.equals("D"))
+			return "Democrat";
+		if (code.equals("R"))
+			return "Republican";
+		if (code.equals("I"))
+			return "Independent";
+		else
+			return "";
+	}
+	
+	private String stateName(String code) {
+		if (code.equals("AL"))
+	        return "Alabama";
+	    if (code.equals("AK"))
+	        return "Alaska";
+	    if (code.equals("AZ"))
+	        return "Arizona";
+	    if (code.equals("AR"))
+	        return "Arkansas";
+	    if (code.equals("CA"))
+	        return "California";
+	    if (code.equals("CO"))
+	        return "Colorado";
+	    if (code.equals("CT"))
+	        return "Connecticut";
+	    if (code.equals("DE"))
+	        return "Delaware";
+	    if (code.equals("DC"))
+	        return "District of Columbia";
+	    if (code.equals("FL"))
+	        return "Florida";
+	    if (code.equals("GA"))
+	        return "Georgia";
+	    if (code.equals("HI"))
+	        return "Hawaii";
+	    if (code.equals("ID"))
+	        return "Idaho";
+	    if (code.equals("IL"))
+	        return "Illinois";
+	    if (code.equals("IN"))
+	        return "Indiana";
+	    if (code.equals("IA"))
+	        return "Iowa";
+	    if (code.equals("KS"))
+	        return "Kansas";
+	    if (code.equals("KY"))
+	        return "Kentucky";
+	    if (code.equals("LA"))
+	        return "Louisiana";
+	    if (code.equals("ME"))
+	        return "Maine";
+	    if (code.equals("MD"))
+	        return "Maryland";
+	    if (code.equals("MA"))
+	        return "Ma  achusetts";
+	    if (code.equals("MI"))
+	        return "Michigan";
+	    if (code.equals("MN"))
+	        return "Minnesota";
+	    if (code.equals("MS"))
+	        return "Mi  i ippi";
+	    if (code.equals("MO"))
+	        return "Mi  ouri";
+	    if (code.equals("MT"))
+	        return "Montana";
+	    if (code.equals("NE"))
+	        return "Nebraska";
+	    if (code.equals("NV"))
+	        return "Nevada";
+	    if (code.equals("NH"))
+	        return "New Hampshire";
+	    if (code.equals("NJ"))
+	        return "New Jersey";
+	    if (code.equals("NM"))
+	        return "New Mexico";
+	    if (code.equals("NY"))
+	        return "New York";
+	    if (code.equals("NC"))
+	        return "North Carolina";
+	    if (code.equals("ND"))
+	        return "North Dakota";
+	    if (code.equals("OH"))
+	        return "Ohio";
+	    if (code.equals("OK"))
+	        return "Oklahoma";
+	    if (code.equals("OR"))
+	        return "Oregon";
+	    if (code.equals("PA"))
+	        return "Pennsylvania";
+	    if (code.equals("PR"))
+	        return "Puerto Rico";
+	    if (code.equals("RI"))
+	        return "Rhode Island";
+	    if (code.equals("SC"))
+	        return "South Carolina";
+	    if (code.equals("SD"))
+	        return "South Dakota";
+	    if (code.equals("TN"))
+	        return "Tenne ee";
+	    if (code.equals("TX"))
+	        return "Texas";
+	    if (code.equals("UT"))
+	        return "Utah";
+	    if (code.equals("VT"))
+	        return "Vermont";
+	    if (code.equals("VA"))
+	        return "Virginia";
+	    if (code.equals("WA"))
+	        return "Washington";
+	    if (code.equals("WV"))
+	        return "West Virginia";
+	    if (code.equals("WI"))
+	        return "Wisconsin";
+	    if (code.equals("WY"))
+	        return "Wyoming";
+	    else
+	        return null;
 	}
 	
 }
