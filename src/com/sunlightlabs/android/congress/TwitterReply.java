921bda646bf8b6c1da2628c062d00b27f2471ecb
klondike <kprojection@gmail.com>
2009-10-02 18:57:05 -0400

    Moved Twitter repies out of the UI thread

--- src/com/sunlightlabs/android/congress/TwitterReply.java
+++ src/com/sunlightlabs/android/congress/TwitterReply.java
@@ -1,8 +1,11 @@
 package com.sunlightlabs.android.congress;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -15,6 +18,8 @@ import com.sunlightlabs.android.twitter.TwitterException;
 
 public class TwitterReply extends Activity {
 	private static final int RESULT_PREFS = 0;
+	
+	private static final int TWEETING = 0; 
 
 	private String username, password;
 	private String tweet_text, tweet_username;
@@ -64,16 +69,48 @@ public class TwitterReply extends Activity {
 	// Gets called after verifyHaveCredentials, which doesn't truly verify that we have credentials - 
 	// it just verifies that if we didn't have them, we brought them to the preference screen once.
 	public void onHaveCredentials() {
-		Twitter twitter = new Twitter(username, password);
-		try {
-			if (twitter.update(message.getText().toString())) {
-				alert("Your Twitter status has been updated.");
-				finish();
-			} else
-				alert("Could not update your Twitter status. Please verify your credentials in the Settings screen.");
-		} catch(TwitterException e) {
-			alert("Could not connect to Twitter. Please try again later.");
-		}
+		postTweet();
+	}
+	
+	final Handler handler = new Handler();
+    final Runnable postSucceeded = new Runnable() {
+        public void run() {
+        	alert("Your Twitter status has been updated.");
+        	dismissDialog(TWEETING);
+			finish();
+        }
+    };
+    final Runnable postFailed = new Runnable() {
+    	public void run() {
+    		alert("Could not update your Twitter status. Please verify your credentials in the Settings screen.");
+    		dismissDialog(TWEETING);
+    	}
+    };
+    final Runnable postError = new Runnable() {
+    	public void run() {
+    		alert("Could not connect to Twitter. Please try again later.");
+    		dismissDialog(TWEETING);
+    	}
+    };
+    
+	
+	public void postTweet() {
+		Thread poster = new Thread() {
+			public void run() {
+				Twitter twitter = new Twitter(username, password);
+				try {
+					if (twitter.update(message.getText().toString()))
+						handler.post(postSucceeded);
+					else
+						handler.post(postFailed);
+				} catch(TwitterException e) {
+					handler.post(postError);
+				}
+			}
+		};
+		poster.run();
+		
+		showDialog(TWEETING);
 	}
 	
 	public void verifyHaveCredentials() {
@@ -98,5 +135,17 @@ public class TwitterReply extends Activity {
 	public void alert(String msg) {
 		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 	}
+	
+	protected Dialog onCreateDialog(int id) {
+        switch(id) {
+        case TWEETING:
+            ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setMessage("Updating your status...");
+            return dialog;
+        default:
+            return null;
+        }
+    }
 	
 }
