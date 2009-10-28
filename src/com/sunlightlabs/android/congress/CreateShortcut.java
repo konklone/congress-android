32d0a4f18772ff47f522531785fe68795a710350
klondike <kprojection@gmail.com>
2009-10-28 16:16:49 -0400

    #25 - Added a CreateShortcut activity that provides a streamlined workflow for adding shortcuts

---
+++ src/com/sunlightlabs/android/congress/CreateShortcut.java
@@ -0,0 +1,62 @@
+package com.sunlightlabs.android.congress;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Window;
+
+public class CreateShortcut extends Activity {
+
+	public static final int RESULT_LASTNAME = 2;
+	private static final int RESULT_SHORTCUT = 10;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        getResponse();
+	}
+	
+	public void getResponse() {
+		Intent intent = new Intent();
+		intent.setClassName("com.sunlightlabs.android.congress", "com.sunlightlabs.android.congress.GetText");
+		Bundle extras = new Bundle();
+		
+		extras.putString("ask", "Enter a last name:");
+		extras.putString("hint", "e.g. Schumer");
+		extras.putInt("inputType", InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+		
+		intent.putExtras(extras);
+		startActivityForResult(intent, RESULT_LASTNAME);
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch(requestCode) {
+		case RESULT_LASTNAME:
+			if (resultCode == RESULT_OK) {
+				String lastName = data.getExtras().getString("response");
+				if (!lastName.equals("")) {
+					Bundle extras = new Bundle();
+					extras.putString("last_name", lastName);
+					Intent i = new Intent();
+					i.setClassName("com.sunlightlabs.android.congress", "com.sunlightlabs.android.congress.LegislatorList");
+					extras.putBoolean("shortcut", true);
+					i.putExtras(extras);
+					startActivityForResult(i, RESULT_SHORTCUT);
+				} else
+					finish();
+			} else
+				finish();
+			break;
+		case RESULT_SHORTCUT:
+			if (resultCode == RESULT_OK) {
+				setResult(RESULT_OK, data);
+	    		finish();
+			}
+			break;
+		}
+	}
+	
+}
