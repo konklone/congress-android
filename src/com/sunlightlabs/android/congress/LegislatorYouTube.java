6d894884ba97cb21918936a49864ab3a2bfaa3f0
klondike <kprojection@gmail.com>
2010-01-08 11:54:13 -0500

    Added high-res icons for all the little things.

--- src/com/sunlightlabs/android/congress/LegislatorYouTube.java
+++ src/com/sunlightlabs/android/congress/LegislatorYouTube.java
@@ -3,7 +3,6 @@ package com.sunlightlabs.android.congress;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
