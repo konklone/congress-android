9d2e16d3fc1bf81413851bbe988d4763c02350dc
klondike <kprojection@gmail.com>
2010-03-01 23:04:23 -0500

    Updated Drumbone URL to include the version string

--- src/com/sunlightlabs/congress/java/Drumbone.java
+++ src/com/sunlightlabs/congress/java/Drumbone.java
@@ -17,8 +17,11 @@ public class Drumbone {
 	public static final String USER_AGENT = "Sunlight's Congress Android App (http://github.com/sunlightlabs/congress)";
 	public static final String[] dateFormat = new String[] {"yy/MM/dd HH:mm:ss Z"};
 	
+	// filled in by client
 	public static String apiKey = "";
 	public static String baseUrl = "";
+	
+	public static String version = "v1";
 	public static String format = "json";
 	
 	
@@ -26,7 +29,7 @@ public class Drumbone {
 		if (queryString.length() > 0)
 			queryString += "&";
 		queryString += "apikey=" + apiKey;
-		return "http://" + baseUrl + "/api/" + method + "." + format + "?" + queryString;
+		return "http://" + baseUrl + "/" + version + "/api/" + method + "." + format + "?" + queryString;
 	}
 	
 	public static String fetchJSON(String url) throws CongressException {
