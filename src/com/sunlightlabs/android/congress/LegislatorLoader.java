8a708329ee84cd540fc0dae4cd31c3c2aca63b82
klondike <kprojection@gmail.com>
2010-07-12 17:55:18 -0400

    Remove popup dialog from LegislatorLoader

--- src/com/sunlightlabs/android/congress/LegislatorLoader.java
+++ src/com/sunlightlabs/android/congress/LegislatorLoader.java
@@ -1,12 +1,9 @@
 package com.sunlightlabs.android.congress;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
-import com.sunlightlabs.android.congress.R;
 import com.sunlightlabs.android.congress.utils.Utils;
 import com.sunlightlabs.congress.models.CongressException;
 import com.sunlightlabs.congress.models.Legislator;
@@ -19,14 +16,15 @@ public class LegislatorLoader extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loading_fullscreen);
 		
-		String legislator_id = getIntent().getStringExtra("legislator_id");
+		String id = getIntent().getStringExtra("legislator_id");
         
         loadLegislatorTask = (LoadLegislatorTask) getLastNonConfigurationInstance();
         if (loadLegislatorTask != null)
         	loadLegislatorTask.onScreenLoad(this);
         else
-        	loadLegislatorTask = (LoadLegislatorTask) new LoadLegislatorTask(this).execute(legislator_id);
+        	loadLegislatorTask = (LoadLegislatorTask) new LoadLegislatorTask(this).execute(id);
 	}
 	
 	@Override
@@ -45,55 +43,30 @@ public class LegislatorLoader extends Activity {
 	
 	private class LoadLegislatorTask extends AsyncTask<String,Void,Legislator> {
 		public LegislatorLoader context;
-		private ProgressDialog dialog = null;
     	
     	public LoadLegislatorTask(LegislatorLoader context) {
     		super();
     		this.context = context;
     		Utils.setupSunlight(context);
     	}
-		
-    	@Override
-    	protected void onPreExecute() {
-    		loadingDialog();
-    	}
     	
     	public void onScreenLoad(LegislatorLoader context) {
     		this.context = context;
-    		loadingDialog();
     	}
     	
     	@Override
-    	protected Legislator doInBackground(String... ids) {
+    	protected Legislator doInBackground(String... id) {
     		try {
-				return LegislatorService.find(ids[0]);
+				return LegislatorService.find(id[0]);
 			} catch(CongressException exception) {
 				return null;
 			}
     	}
     	
     	@Override
-    	protected void onPostExecute(Legislator legislator) {
-    		if (dialog != null && dialog.isShowing())
-        		dialog.dismiss();
-    		
+    	protected void onPostExecute(Legislator legislator) {    		
     		context.onLoadLegislator(legislator);
     		context.loadLegislatorTask = null;
-    	}
-    	
-    	private void loadingDialog() {
-    		dialog = new ProgressDialog(context);
-            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            dialog.setMessage("Loading legislator...");
-            
-            dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-				public void onCancel(DialogInterface dialog) {
-					cancel(true);
-					context.finish();
-				}
-			});
-            
-    		dialog.show();
     	}
     }
 }
