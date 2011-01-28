88d91df48f35d7c0c16cd7ed32c2b9f2a60fd02c
Eric Mill <kprojection@gmail.com>
2011-01-28 12:12:17 -0500

    Fixed clickable 'Powered by Google' header view on newslist

--- src/com/sunlightlabs/android/congress/NewsList.java
+++ src/com/sunlightlabs/android/congress/NewsList.java
@@ -97,7 +97,7 @@ public class NewsList extends ListActivity implements LoadsNews {
 	private void setupControls() {
 		TextView header = (TextView) LayoutInflater.from(this).inflate(R.layout.list_header_simple, null);
 		header.setText(R.string.google_news_branding);
-		getListView().addHeaderView(header);
+		getListView().addHeaderView(header, null, false);
 		
 		Utils.setLoading(this, R.string.news_loading);
 		((Button) findViewById(R.id.refresh)).setOnClickListener(new View.OnClickListener() {
