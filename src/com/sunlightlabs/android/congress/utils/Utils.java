ec7a494edd47f9ebf8f6635316d7ae7c22dc19c1
Eric Mill <kprojection@gmail.com>
2011-05-19 22:28:47 -0400

    Removed use of deprecated constant

--- src/com/sunlightlabs/android/congress/utils/Utils.java
+++ src/com/sunlightlabs/android/congress/utils/Utils.java
@@ -36,7 +36,7 @@ public class Utils {
 		RealTimeCongress.userAgent = resources.getString(R.string.rtc_user_agent);
 		RealTimeCongress.apiKey = resources.getString(R.string.sunlight_api_key);
 		RealTimeCongress.appVersion = resources.getString(R.string.app_version);
-		RealTimeCongress.osVersion = "Android " + Build.VERSION.SDK;
+		RealTimeCongress.osVersion = "Android " + Build.VERSION.SDK_INT;
 	}
 
 	public static void setupSunlight(Context context) {
