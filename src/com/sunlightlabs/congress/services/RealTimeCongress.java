444448edbc6b31a6e85621a52033ca4f4554a896
Eric Mill <kprojection@gmail.com>
2011-06-14 16:29:59 -0400

    Whoops, committed staging URL - I knew that would happen

--- src/com/sunlightlabs/congress/services/RealTimeCongress.java
+++ src/com/sunlightlabs/congress/services/RealTimeCongress.java
@@ -34,7 +34,7 @@ public class RealTimeCongress {
 	public static final String dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 	public static final String dateOnlyFormat = "yyyy-MM-dd";
 	
-	public static final String baseUrl = "http://rtc.sunlightlabs.com/api/v1/";
+	public static final String baseUrl = "http://api.realtimecongress.org/api/v1/";
 	public static final String format = "json";
 	
 	public static final String highlightTags = "<b>,</b>"; // default highlight tags
