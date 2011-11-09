3575c2d8c3f844c2d648e0edd003bbc1edee4a27
Eric Mill <kprojection@gmail.com>
2011-11-09 11:20:36 -0500

    Moved stuff into a function

--- src/com/sunlightlabs/android/congress/CommitteeList.java
+++ src/com/sunlightlabs/android/congress/CommitteeList.java
@@ -46,13 +46,25 @@ public class CommitteeList extends FragmentActivity {
 		chamber = extras.getString("chamber");
 		legislator = (Legislator) extras.getSerializable("legislator");
 		
-		setupControls();
-		
 		if (type == CHAMBER)
 			Analytics.track(this, "/committees/" + chamber);
 		else if (type == LEGISLATOR)
 			Analytics.track(this, "/legislator/" + legislator.id + "/committees");
-			
+		
+		setupControls();
+		setupFragments();
+	}
+	
+	private void setupControls() {
+		if (type == LEGISLATOR) {
+			setContentView(R.layout.frame_titled);
+			Utils.setTitle(this, "Committees for " + legislator.titledName(), R.drawable.committees);
+			Utils.setTitleSize(this, 16);
+		} else
+			setContentView(R.layout.frame);
+	}
+	
+	private void setupFragments() {
 		if (getSupportFragmentManager().findFragmentById(R.id.frame) == null) {
 			CommitteeListFragment list;
 			
@@ -64,17 +76,6 @@ public class CommitteeList extends FragmentActivity {
             getSupportFragmentManager().beginTransaction().add(R.id.frame, list).commit();
         }
 	}
-	
-	private void setupControls() {
-		if (type == LEGISLATOR) {
-			setContentView(R.layout.frame_titled);
-			Utils.setTitle(this, "Committees for " + legislator.titledName(), R.drawable.committees);
-			Utils.setTitleSize(this, 18);
-		} else
-			setContentView(R.layout.frame);
-	}
-	
-	
 	
 	static class CommitteeListFragment extends ListFragment {
 		public static final int CHAMBER = 1;
