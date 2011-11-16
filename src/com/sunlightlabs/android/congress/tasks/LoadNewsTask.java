229988ccab7af355bfb784fed6804bd65c31d649
Eric Mill <kprojection@gmail.com>
2011-11-16 15:03:44 -0500

    Order news results by date - still need some highlighting for new items, since the order is chaotic, but this will help

--- src/com/sunlightlabs/android/congress/tasks/LoadNewsTask.java
+++ src/com/sunlightlabs/android/congress/tasks/LoadNewsTask.java
@@ -1,5 +1,7 @@
 package com.sunlightlabs.android.congress.tasks;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import android.os.AsyncTask;
@@ -31,7 +33,14 @@ public class LoadNewsTask extends AsyncTask<String, Void, List<NewsItem>> {
 		String referer = params[2];
 		
 		try {
-			return new NewsService(apiKey, referer).fetchNewsResults(searchTerm);
+			List<NewsItem> results = new NewsService(apiKey, referer).fetchNewsResults(searchTerm);
+			Collections.sort(results, new Comparator<NewsItem>() {
+				@Override
+				public int compare(NewsItem a, NewsItem b) {
+					return b.timestamp.compareTo(a.timestamp);
+				}
+			});
+			return results;
 		} catch (NewsException exception) {
 			this.exception = new CongressException(exception, "Exception fetching news");
 			return null;
