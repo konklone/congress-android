ad500a202e36d8aea7fd490fa43009fc3372dfc0
Eric Mill <kprojection@gmail.com>
2012-01-13 18:44:51 -0500

    Unused import

--- src/com/sunlightlabs/android/congress/NotificationSettings.java
+++ src/com/sunlightlabs/android/congress/NotificationSettings.java
@@ -14,7 +14,6 @@ import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.TextView;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
