b6424dd46c09ed8a8c2a4a94ccbd06caff3651cb
Eric Mill <konklone@gmail.com>
2013-03-06 17:41:09 -0500

    Upgrading WakefulIntentService to the latest version, losing the wifi lock for now

--- src/com/commonsware/cwac/wakeful/WakefulIntentService.java
+++ src/com/commonsware/cwac/wakeful/WakefulIntentService.java
@@ -1,116 +1,124 @@
 /***
-	Copyright (c) 2009 CommonsWare, LLC
-	
-	Licensed under the Apache License, Version 2.0 (the "License"); you may
-	not use this file except in compliance with the License. You may obtain
-	a copy of the License at
-		http://www.apache.org/licenses/LICENSE-2.0
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+  Copyright (c) 2009-11 CommonsWare, LLC
+  
+  Licensed under the Apache License, Version 2.0 (the "License"); you may
+  not use this file except in compliance with the License. You may obtain
+  a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  */
 
 package com.commonsware.cwac.wakeful;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.net.wifi.WifiManager;
+import android.content.SharedPreferences;
 import android.os.PowerManager;
 
 abstract public class WakefulIntentService extends IntentService {
-	abstract protected void doWakefulWork(Intent intent);
+  abstract protected void doWakefulWork(Intent intent);
 
-	public static final String LOCK_NAME_STATIC = "com.commonsware.cwac.wakeful.WakefulIntentService";
-	private static PowerManager.WakeLock lockCpu = null;
-	private static WifiManager.WifiLock lockWifi = null;
+  static final String NAME=
+      "com.commonsware.cwac.wakeful.WakefulIntentService";
+  static final String LAST_ALARM="lastAlarm";
+  private static volatile PowerManager.WakeLock lockStatic=null;
 
-	public static void acquireStaticLock(Context context) {
-		if (!getCpuLock(context).isHeld())
-			getCpuLock(context).acquire();
+  synchronized private static PowerManager.WakeLock getLock(Context context) {
+    if (lockStatic == null) {
+      PowerManager mgr=
+          (PowerManager)context.getSystemService(Context.POWER_SERVICE);
 
-		WifiManager.WifiLock lock = getWifiLock(context);
-		try {
-			if (!lock.isHeld())
-				lock.acquire();
-		}
-		// too many wifi locks, couldn't acquire one
-		catch (UnsupportedOperationException ex) {
-			// swallow it. oh well, no wifi lock this time.
-		}
-	}
+      lockStatic=mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, NAME);
+      lockStatic.setReferenceCounted(true);
+    }
 
-	synchronized protected static PowerManager.WakeLock getCpuLock(Context context) {
-		if (lockCpu == null) {
-			PowerManager mgr = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+    return(lockStatic);
+  }
 
-			// wake up the CPU
-			lockCpu = mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,LOCK_NAME_STATIC);
-			lockCpu.setReferenceCounted(true);
-		}
+  public static void sendWakefulWork(Context ctxt, Intent i) {
+    getLock(ctxt.getApplicationContext()).acquire();
+    ctxt.startService(i);
+  }
 
-		return lockCpu;
-	}
+  public static void sendWakefulWork(Context ctxt, Class<?> clsService) {
+    sendWakefulWork(ctxt, new Intent(ctxt, clsService));
+  }
 
-	synchronized protected static WifiManager.WifiLock getWifiLock(Context context) {
-		if (lockWifi == null) {
-			WifiManager mgr = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+  public static void scheduleAlarms(AlarmListener listener, Context ctxt) {
+    scheduleAlarms(listener, ctxt, true);
+  }
 
-			// wake up the WiFi
-			lockWifi = mgr.createWifiLock(LOCK_NAME_STATIC);
-			lockWifi.setReferenceCounted(true);
-		}
+  public static void scheduleAlarms(AlarmListener listener,
+                                    Context ctxt, boolean force) {
+    SharedPreferences prefs=ctxt.getSharedPreferences(NAME, 0);
+    long lastAlarm=prefs.getLong(LAST_ALARM, 0);
 
-		return lockWifi;
-	}
+    if (lastAlarm == 0
+        || force
+        || (System.currentTimeMillis() > lastAlarm && System.currentTimeMillis()
+            - lastAlarm > listener.getMaxAge())) {
+      AlarmManager mgr=
+          (AlarmManager)ctxt.getSystemService(Context.ALARM_SERVICE);
+      Intent i=new Intent(ctxt, AlarmReceiver.class);
+      PendingIntent pi=PendingIntent.getBroadcast(ctxt, 0, i, 0);
 
-	public static void sendWakefulWork(Context ctxt, Intent i) {
-		acquireStaticLock(ctxt);
-		ctxt.startService(i);
-	}
+      listener.scheduleAlarms(mgr, pi, ctxt);
+    }
+  }
 
-	public static void sendWakefulWork(Context ctxt, Class<?> clsService) {
-		sendWakefulWork(ctxt, new Intent(ctxt, clsService));
-	}
+  public static void cancelAlarms(Context ctxt) {
+    AlarmManager mgr=
+        (AlarmManager)ctxt.getSystemService(Context.ALARM_SERVICE);
+    Intent i=new Intent(ctxt, AlarmReceiver.class);
+    PendingIntent pi=PendingIntent.getBroadcast(ctxt, 0, i, 0);
 
-	public WakefulIntentService(String name) {
-		super(name);
-	}
+    mgr.cancel(pi);
+  }
 
-	@Override
-	public void onStart(Intent intent, int startId) {
-		if (!getCpuLock(this).isHeld()) // fail-safe for crash restart
-			getCpuLock(this).acquire();
-		
-		if (!getWifiLock(this).isHeld()) 
-			getWifiLock(this).acquire();
-		
-		super.onStart(intent, startId);
-	}
+  public WakefulIntentService(String name) {
+    super(name);
+    setIntentRedelivery(true);
+  }
 
-	@Override
-	public void onDestroy() {
-		// must release locks when the service is destroyed, otherwise will drain the battery
-		if (getCpuLock(this).isHeld())
-			getCpuLock(this).release();
-		
-		if (getWifiLock(this).isHeld())
-			getWifiLock(this).release();
-		
-		super.onDestroy();
-	}
+  @Override
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    PowerManager.WakeLock lock=getLock(this.getApplicationContext());
 
-	@Override
-	protected void onHandleIntent(Intent intent) {
-		try {
-			doWakefulWork(intent);
-		} finally {
-			if (getCpuLock(this).isHeld())
-				getCpuLock(this).release();
-			if (getWifiLock(this).isHeld())
-				getWifiLock(this).release();
-		}
-	}
+    if (!lock.isHeld() || (flags & START_FLAG_REDELIVERY) != 0) {
+      lock.acquire();
+    }
+
+    super.onStartCommand(intent, flags, startId);
+
+    return(START_REDELIVER_INTENT);
+  }
+
+  @Override
+  final protected void onHandleIntent(Intent intent) {
+    try {
+      doWakefulWork(intent);
+    }
+    finally {
+      PowerManager.WakeLock lock=getLock(this.getApplicationContext());
+      
+      if (lock.isHeld()) {
+        lock.release();
+      }
+    }
+  }
+
+  public interface AlarmListener {
+    void scheduleAlarms(AlarmManager mgr, PendingIntent pi, Context ctxt);
+
+    void sendWakefulWork(Context ctxt);
+
+    long getMaxAge();
+  }
 }
