c47f65eda09cd401aeff3ead5746d57af4ef69b9
Eric Mill <eric@konklone.com>
2017-07-23 15:01:54 -0400

    disable use of offset while PP figures out some server issues

--- app/src/main/java/com/sunlightlabs/congress/services/ProPublica.java
+++ app/src/main/java/com/sunlightlabs/congress/services/ProPublica.java
@@ -71,10 +71,11 @@ public class ProPublica {
         Map<String,String> params = new HashMap<String,String>();
 
         // Only use for query string is an "offset" for pagination as needed
-        if (page > 0) {
-            int offset = (page - 1) * PPPP;
-            params.put("offset", String.valueOf(offset));
-        }
+        // TODO: disable for now while it causes problems
+//        if (page > 0) {
+//            int offset = (page - 1) * PPPP;
+//            params.put("offset", String.valueOf(offset));
+//        }
 
         StringBuilder query = new StringBuilder("");
         Iterator<String> iterator = params.keySet().iterator();
